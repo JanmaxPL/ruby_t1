34105004071f91da1e1d713df957fef87daf09cf
EC2 Default User <ec2-user@ip-172-31-4-203.us-east-2.compute.internal>
2018-10-12 17:44:51 +0000

    witaj swiecie.jp2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hello, world! Witaj Świecie ¡Hola, mundo!"
+    render html: "Hello, world! Witaj Świecie ¡Hola, mundo! こんにちは！"
   end
   
 end
